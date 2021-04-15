df57ddbbaca020be1fc56665cfe291fed843b4b9
Alejandro Garcia Paterna <agp164@mscloud.ua.es>
2021-04-15 19:40:22 +0200

    Pedido terminado

--- WabiSabiLibrary/CADPedido.cs
+++ WabiSabiLibrary/CADPedido.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,5 +40,128 @@ namespace WabiSabiLibrary
 
             return true;
         }
+        public bool updateUsuario(ENPedido en)
+        {
+            SqlConnection conn = null;
+            String comando = "Update Pedido set fecha = '" + en.Fecha + "', precio = '" + en.Precio + "', Direccion = '" + en.Direccion + "', Estado = '" + en.Estado
+                + "' where id = '" + en.Id + "'";
+
+            try
+            {
+                conn = new SqlConnection(constring);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            finally
+            {
+                if (conn != null) conn.Close(); // Se asegura de cerrar la conexión.
+            }
+
+            return true;
+        }
+
+        public void readPedido(ENPedido en)
+        {
+            SqlConnection conn = null;
+            String comando = "Select * from Pedido where Id = '" + en.Id + "'";
+
+            try
+            {
+                conn = new SqlConnection(constring);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    en.Fecha = dr.GetDateTime(1);
+                    en.Precio = dr.GetFloat(2);
+                    en.Direccion = dr.GetString(3);
+                    en.Estado = dr.GetString(4);
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+                dr.Close();
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            finally
+            {
+                if (conn != null) conn.Close(); // Se asegura de cerrar la conexión.
+            }
+        }
+        public void confirmPedido(ENPedido en)
+        {
+            SqlConnection conn = null;
+            String comando = "Select * from Pedido where Id = '" + en.Id + "'";
+
+            try
+            {
+                conn = new SqlConnection(constring);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    en.Fecha = dr.GetDateTime(1);
+                    en.Precio = dr.GetFloat(2);
+                    en.Direccion = dr.GetString(3);
+                    en.Estado = dr.GetString(4);
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+                dr.Close();
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            finally
+            {
+                if (conn != null) conn.Close(); // Se asegura de cerrar la conexión.
+            }
+        }
+        public bool deletePedido(ENPedido en)
+        {
+            SqlConnection conn = null;
+            String comando = "Delete from Pedido where Id = '" + en.Id + "'";
+
+            try
+            {
+                conn = new SqlConnection(constring);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            finally
+            {
+                if (conn != null) conn.Close(); // Se asegura de cerrar la conexión.
+            }
+
+            return true;
+        }
     }
+
 }

3b3365064bf996d0a9661f9177e14988d43e6db7
rociogarciarobles <rociogarciarobles>
2021-04-15 17:55:33 +0200

    "53247879G creacion de EnMenu y CADMenu"

---
+++ WabiSabiLibrary/CADMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace WabiSabiLibrary
+{
+        public class CADMenu
+        {
+	        public CADMenu()
+	        {
+                private string constring;
+
+                public CADMenu()
+                {
+                    constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
+                }
+
+                public bool createMenu(ENMenu en)
+                {
+                    SqlConnection conexion = new SqlConnection(constring);
+                    string comando = "Insert Into Menu (id, precio, nombre) " + "VALUES ('" + en.id + "', '" + en.precio + "', '" + en.nombre + "')";
+                    try
+                    {
+                        conexion.Open();
+                        SqlCommand cmd = new SqlCommand(comando, conexion);
+                        cmd.ExecuteNonQuery();
+                        conexion.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        conexion.Close();
+                        Console.WriteLine("Error: " + e);
+                        return false;
+                    }
+                    return true;
+                }
+        }
+}
---
+++ WabiSabiLibrary/ENMenu.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WabiSabiLibrary
+{
+	public class ENMenu
+	{
+		public class ENMenu()
+			{
+				private int id;
+		private float precio;
+		private string nombre;
+
+		public int id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+
+		public float precio
+		{
+			get { return precio; }
+			set { precio = value; }
+		}
+
+		public string nombre
+		{
+			get { return estado; }
+			set { nombre = value; }
+		}
+
+		public ENMenu()
+		{
+			id = 0;
+			precio = 0.0;
+			nombre = "";
+		}
+
+		public ENMenu(int id, float precio, string nombre)
+		{
+			this.id = id;
+			this.precio = precio;
+			this.direccion = nombre;
+		}
+
+		public void create()
+		{
+			CADMenu cad = new CADMenu();
+			cad.create(this);
+		}
+
+		public void delete()
+		{
+			CADMenu cad = new CADMenu();
+			cad.delete(this);
+		}
+
+		public void update()
+		{
+			CADMenu cad = new CADMenu();
+			cad.update(this);
+		}
+	}
+}

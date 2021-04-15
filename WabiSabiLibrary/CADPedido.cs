726ca8f7c8ad98ad8f1ef2e475e328e00d4eb23c
Alejandro Garcia Paterna <agp164@mscloud.ua.es>
2021-04-15 15:19:11 +0200

    Clases

---
+++ WabiSabiLibrary/CADPedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WabiSabiLibrary
+{
+    class CADPedido
+    {
+        private string constring { get; set; }
+
+        public CADPedido()
+        {
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+    }
+}

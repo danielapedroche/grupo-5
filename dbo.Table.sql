ad8a2bea9b10106edbff600c2887905819637ac7
fgm99 <63115564+fgm99@users.noreply.github.com>
2021-04-16 11:13:20 +0200

    todo el proyecto

---
+++ dbo.Table.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[Productos]
+(
+	[codigo] INT NOT NULL PRIMARY KEY,
+	[nombre] NVARCHAR (32) NOT NULL,
+	[descripcion] NVARCHAR (64) NOT NULL,
+	[precio] FLOAT NOT NULL,
+    UNIQUE NONCLUSTERED ([nombre] ASC)
+)
+CREATE TABLE [dbo].[Ofertas]
+(
+	[codigo] INT NOT NULL PRIMARY KEY,
+	[tipo] INT NOT NULL,
+	[descripcion] NVARCHAR (64) NOT NULL,
+    UNIQUE NONCLUSTERED ([nombre] ASC)
+)
+CREATE TABLE [dbo].[Usuarios]
+(
+	[nif] NVARCHAR (32) NOT NULL PRIMARY KEY,
+	[email] INT NOT NULL PRIMARY KEY,
+	[contrasenya] INT NOT NULL PRIMARY KEY,
+	[nombre] INT NOT NULL PRIMARY KEY,
+	[apellidos] INT NOT NULL PRIMARY KEY,
+	[telefono] NVARCHAR (32) NOT NULL,
+	[direccion] NVARCHAR (64) NOT NULL,
+	[fechanac] DATE NOT NULL,
+    UNIQUE NONCLUSTERED ([nombre] ASC)
+)

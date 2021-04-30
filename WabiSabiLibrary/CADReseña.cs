9591f77a6d8f7b099dd574d3759311044915e7ec
alexgp98 <32191893+alexgp98@users.noreply.github.com>
2021-04-30 09:22:10 +0200

    Add files via upload

---
+++ WabiSabiLibrary/CADReseña.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WabiSabiLibrary
+{
+    class CADReseña
+    {
+        private string constring;
+        public CADReseña()
+        {
+            constring = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ToString();
+        }
+        public bool create(ENReseña en)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            string comando = "Insert Into [dbo].[Reseñas] (id, contenido) " + "VALUES ('" + en.Id + "', '" + en.Contenido + ")";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                conn.Close();
+                Console.WriteLine("Error: " + e);
+                return false;
+            }
+            return true;
+        }
+        public bool read(ENReseña en)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            String comando = "select * from [dbo].[Reseñas] where id='" + en.Id + "'";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                if (int.Parse(dr["id"].ToString()) == en.Id)
+                {
+                    en.Id = int.Parse(dr["id"].ToString());
+                    en.Contenido = dr["contenido"].ToString();
+                    dr.Close();
+                    conn.Close();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+        }
+        public bool update(ENReseña en)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            string comando = "UPDATE [dbo].[Reseñas] " + "SET contenido = '" + en.Contenido + "where id ='" + en.Id + "'";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return true;
+        }
+        public bool delete(ENReseña en)
+        {
+            SqlConnection conn = new SqlConnection(constring); ;
+            string comando = "Delete from [dbo].[Reseña] where id = '" + en.Id + "'";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return true;
+        }
+    }
+}

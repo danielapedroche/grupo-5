a475725ad2305ce5188105607f907c332cfc956b
fgm99 <63115564+fgm99@users.noreply.github.com>
2021-05-03 13:07:42 +0200

    resubida fran

---
+++ WabiSabiLibrary/CAD/CADAlergenos.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace WabiSabiLibrary
+{
+    class CADAlergenos
+    {
+        private string constring;
+        public CADAlergenos()
+        {
+            constring = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ToString();
+        }
+        public bool create(ENAlergenos en)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            string comando = "Insert Into [dbo].[Alergenos] (codigo, nombre, descripcion) " + "VALUES ('" + en.Codigo + "', '" + en.Nombre + ")";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                conn.Close();
+                Console.WriteLine("Error: " + e);
+                return false;
+            }
+            return true;
+        }
+        public bool read(ENAlergenos en)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            String comando = "select * from [dbo].[Alergenos] where codigo='" + en.Codigo + "'";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                if (int.Parse(dr["codigo"].ToString()) == en.Codigo)
+                {
+                    en.Codigo = int.Parse(dr["codigo"].ToString());
+                    en.Nombre = dr["nombre"].ToString();
+                    en.Descripcion = dr["descripcion"].ToString();
+                    dr.Close();
+                    conn.Close();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+        }
+        public bool update(ENAlergenos en)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            string comando = "UPDATE [dbo].[Alergenos] " + "SET nombre = '" + en.Nombre + "',  descripcion" + en.Descripcion + "where codigo ='" + en.Codigo + "'";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return true;
+        }
+        public bool delete(ENAlergenos en)
+        {
+            SqlConnection conn = new SqlConnection(constring); ;
+            string comando = "Delete from [dbo].[Alergenos] where codigo = '" + en.Codigo + "'";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(comando, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return true;
+        }
+    }
+}
